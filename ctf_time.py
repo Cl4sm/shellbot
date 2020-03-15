0fced9081ae5a76bcad012016a6afd96652280ca
Kyle ZENG <jkjh1jkjh1@gmail.com>
2020-03-15 13:22:04 -0700

    let's write less ugly code

--- ctf_time.py
+++ ctf_time.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
-import requests
+import time
 import datetime
+
+import requests
 import pprint
 
-
+DEFAULT_DELTA = datetime.timedelta(days=7).total_seconds()
 pp = pprint.PrettyPrinter(indent=4)
 
 
 class CTFTime:
 
-    def __init__(self, limit=10, start=int(datetime.datetime.today().timestamp()), finish=int((datetime.datetime.today() + datetime.timedelta(days=7)).timestamp())):
+    def __init__(self, limit=10, start=time.time(), finish=time.time() + DEFAULT_DELTA):
         self.limit = limit
-        self.start = start
-        self.finish = finish
+        self.start = int(start)
+        self.finish = int(finish)
     
     @property
     def upcoming_ctfs(self):
+        params = {'limit': self.limit,
+                  'start': self.start,
+                  'finish': self.finish}
+        headers = {'User-Agent': 'curl/7.58.0',
+                   'Host': 'ctftime.org'}
+
         response = requests.get('https://ctftime.org/api/v1/events/',
-                    params={'limit': self.limit, 'start': self.start, 'finish': self.finish},
-                    headers = {
-                        'User-Agent': 'curl/7.58.0', 
-                        'Host': 'ctftime.org',
-                        })
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception('CTFTime', 'Could not get upcoming ctfs')
+                                params=params,
+                                headers=headers)
+
+        assert response.status_code == 200, "Could not get upcoming ctfs"
+
+        return response.json()
+
+if __name__ == '__main__':
+    ctftime = CTFTime()
+    pp.pprint(ctftime.upcoming_ctfs)
